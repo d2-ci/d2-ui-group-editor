27ff7fd3faba983568932fe1b730b8e5fd6a0a8a
joakimia <joakimamundsen91@gmail.com>
2019-06-25 09:30:38 +0000

    b5c5212 adding tests for new epi weeks alternatives

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _GroupEditor = require('./GroupEditor.component');
+
+Object.defineProperty(exports, 'GroupEditor', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_GroupEditor).default;
+  }
+});
+
+var _GroupEditorWithOrdering = require('./GroupEditorWithOrdering.component');
+
+Object.defineProperty(exports, 'GroupEditorWithOrdering', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_GroupEditorWithOrdering).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
